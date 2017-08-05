73d20a788a26e433cd4b3a02e462dde94a67c095
Yurii Boiko <yurii.boiko@globallogic.com>
2017-08-06 02:40:40 +0300

    todo

--- QuickFile/Controllers/YSDriveViewController.swift
+++ QuickFile/Controllers/YSDriveViewController.swift
@@ -18,7 +18,7 @@ class YSDriveViewController: UITableViewController
     weak var toolbarView: YSToolbarView!
     
     var selectedIndexes : [IndexPath] = []
-    //TODO: make relative urls, pagination, logged as, download wifi only, do not delete files from remote even if they are deleted
+    //TODO: make relative urls, pagination, logged as, download wifi only, do not delete files from remote even if they are deleted, disallow refreshes in edit
     var viewModel: YSDriveViewModelProtocol?
     {
         willSet
